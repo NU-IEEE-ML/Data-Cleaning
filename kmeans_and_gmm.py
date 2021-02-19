9df5756ac2a43ed92c70f73e864aca7cb18c5541
Misha Ivaniuk <mykhailo.ivaniuk@gmail.com>
2021-02-18 22:18:00 -0600

    Silhouette score check added for Kmeans

--- kmeans_and_gmm.py
+++ kmeans_and_gmm.py
@@ -1,10 +1,14 @@
 import numpy as np
 import sklearn as sk
 import sklearn.model_selection as model_selection
-import sklearn.cluster as cluster
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
 import pandas as pd
-from sklearn import preprocessing
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error,silhouette_score,silhouette_samples
+
+
 def data_split(path):
     data = pd.read_csv(path)
     train,test = model_selection.train_test_split(data,test_size=0.25,random_state=1)
@@ -12,13 +16,20 @@ def data_split(path):
 
 def kmeans(path,n_clusters):
     train,test = data_split(path)
-    kmeans = cluster.KMeans(n_clusters=n_clusters, algorithm='full')
+    kmeans = KMeans(n_clusters=n_clusters, algorithm='full')
     kmeans.fit(train)
     preds = kmeans.predict(test)
-    mse = mean_squared_error(test['price'],preds)
-    preds_rmse = mse **(1/2)
-    print(preds_rmse)
-    print(mse)
+    range_n_clusters = [6,8,10,12,14,16,18,20,22]
+    # for i in range_n_clusters:
+    #     kmeans = KMeans(n_clusters=i)
+    #     cluster_labels = kmeans.fit_predict(train)
+    #     silhouette_avg = silhouette_score(train, cluster_labels)
+    #     print("For n_clusters =", i,
+    #           "The average silhouette_score is :", silhouette_avg)
+    # mse = mean_squared_error(test['price'],preds)
+    # preds_rmse = mse **(1/2)
+    # print(preds_rmse)
+    # print(mse)
 
 
-kmeans('cleaned_data.csv',4)
+kmeans('cleaned_data.csv',6)
